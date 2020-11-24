acad49a4a4343dba3a60d01a0ff13a3859d6c527
mary <kiseleva15@gmail.com>
2020-11-24 19:50:35 +0200

    removed test lines

--- lesson10/window_func.sql
+++ lesson10/window_func.sql
@@ -6,8 +6,8 @@
 -- общее количество пользователей в группе
 -- всего пользователей в системе
 -- отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100
-
 USE vk;
+
 SELECT DISTINCT c.name,
     ROUND(COUNT(cu.user_id) OVER() / number_of_communities, 2) AS avg,
 	FIRST_VALUE(CONCAT_WS(' ', u.first_name, u.last_name, '-', CAST(TIMESTAMPDIFF(YEAR, p.birthday, CURDATE()) AS char))) OVER w_1 AS youngest_cu,
@@ -27,13 +27,7 @@ SELECT DISTINCT c.name,
       ON cu.user_id = u.id
   WINDOW w_1 AS (PARTITION BY c.id ORDER BY p.birthday DESC),
     w_2 AS (PARTITION BY c.id ORDER BY p.birthday); 
-      
-SELECT * FROM users LIMIT 10; 
-SELECT COUNT(*) FROM communities_users;
 
-SELECT CONCAT_WS(' ', 'FILD', CAST(TIMESTAMPDIFF(YEAR, birthday, CURDATE()) AS char(255))) AS P1
-  FROM profiles  
-	LIMIT 1;
 
 
 
